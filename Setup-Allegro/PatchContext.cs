17354b14e692e209a27498eae869c4733afaf543
Jeff <e8035669@gmail.com>
2019-11-12 01:18:28 +0800

    init

---
+++ Setup-Allegro/PatchContext.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Setup_Allegro {
+    static class PatchContext {
+        public static string gccPatch = @"
+			<gcc>
+				<NAME>
+					<str>
+						<![CDATA[GNU GCC Compiler]]>
+					</str>
+				</NAME>
+				<LINKER_OPTIONS>
+					<str>
+						<![CDATA[-static;]]>
+					</str>
+				</LINKER_OPTIONS>
+				<INCLUDE_DIRS>
+					<str>
+						<![CDATA[C:\API\Allegro_5.2.5.1\include;]]>
+					</str>
+				</INCLUDE_DIRS>
+				<LIBRARY_DIRS>
+					<str>
+						<![CDATA[C:\API\Allegro_5.2.5.1\lib;]]>
+					</str>
+				</LIBRARY_DIRS>
+				<LIBRARIES>
+					<str>
+						<![CDATA[liballegro_monolith-static.a;libdumb.a;libFLAC.a;libfreetype.a;libjpeg.a;libopusfile.a;libopus.a;libphysfs.a;libpng16.a;libtheoradec.a;libvorbisfile.a;libvorbis.a;libogg.a;libwebp.a;libwebpdecoder.a;libwebpdemux.a;libzlib.a;libpsapi.a;libwinmm.a;libshlwapi.a;libgdi32.a;libopengl32.a;libole32.a;libdsound.a;libpthread.a;libkernel32.a;libuser32.a;libwinspool.a;libshell32.a;liboleaut32.a;libuuid.a;libcomdlg32.a;libadvapi32.a;]]>
+					</str>
+				</LIBRARIES>
+				<MASTER_PATH>
+					<str>
+						<![CDATA[C:\Compiler\MinGW_8.2.1]]>
+					</str>
+				</MASTER_PATH>
+				<C_COMPILER>
+					<str>
+						<![CDATA[gcc.exe]]>
+					</str>
+				</C_COMPILER>
+				<CPP_COMPILER>
+					<str>
+						<![CDATA[g++.exe]]>
+					</str>
+				</CPP_COMPILER>
+				<LINKER>
+					<str>
+						<![CDATA[g++.exe]]>
+					</str>
+				</LINKER>
+				<DEBUGGER_CONFIG>
+					<str>
+						<![CDATA[gdb_debugger:Default]]>
+					</str>
+				</DEBUGGER_CONFIG>
+			</gcc>
+";
+
+        public static string gdbPatch = @"
+				<conf1>
+					<NAME>
+						<str>
+							<![CDATA[Default]]>
+						</str>
+					</NAME>
+					<values>
+						<EXECUTABLE_PATH>
+							<str>
+								<![CDATA[C:\Compiler\MinGW_8.2.1\bin\gdb.exe]]>
+							</str>
+						</EXECUTABLE_PATH>
+						<DISABLE_INIT bool=""1"" />
+						<USER_ARGUMENTS>
+							<str>
+								<![CDATA[]]>
+							</str>
+						</USER_ARGUMENTS>
+						<TYPE int=""0"" />
+						<INIT_COMMANDS>
+							<str>
+								<![CDATA[]]>
+							</str>
+						</INIT_COMMANDS>
+						<WATCH_ARGS bool=""1"" />
+						<WATCH_LOCALS bool=""1"" />
+						<CATCH_EXCEPTIONS bool=""1"" />
+						<EVAL_TOOLTIP bool=""0"" />
+						<ADD_OTHER_SEARCH_DIRS bool=""0"" />
+						<DO_NOT_RUN bool=""0"" />
+						<DISASSEMBLY_FLAVOR int=""0"" />
+						<INSTRUCTION_SET>
+							<str>
+								<![CDATA[]]>
+							</str>
+						</INSTRUCTION_SET>
+					</values>
+				</conf1>
+";
+    }
+}
--- Setup-Allegro/Program.cs
+++ Setup-Allegro/Program.cs
@@ -3,10 +3,202 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net;
+using System.IO;
+using System.IO.Compression;
+using System.Xml.Linq;
+using System.Xml;
 
 namespace Setup_Allegro {
     class Program {
+        private static string allegroUri = "https://ical168.ddns.net/learning.ical.tw/files/allegro-x86_64-w64-mingw32-gcc-8.2.1-posix-seh-static-5.2.5.1.zip";
+        private static string allegroPath = "Allegro.zip";
+        private static string allegroUnzipPath = @".\";
+        private static string allegroSrcPath = @".\allegro-x86_64-w64-mingw32-gcc-8.2.1-posix-seh-static-5.2.5.1";
+        private static string allegroDestPath = @"C:\API\Allegro_5.2.5.1";
+
+        private static string compilerUri = "https://ical168.ddns.net/learning.ical.tw/files/x86_64-w64-mingw32-gcc-8.2.1-posix-seh.zip";
+        private static string compilerPath = "compiler.zip";
+        private static string compilerUnzipPath = @".\";
+        private static string compilerSrcPath = @".\mingw64";
+        private static string compilerDestPath = @"C:\Compiler\MinGW_8.2.1";
+
+        private static WebClient client = new WebClient();
+        private static string currentFileName = "";
+
         static void Main(string[] args) {
+            client.DownloadProgressChanged += Client_DownloadProgressChanged;
+
+            downloadFiles(client, allegroUri, allegroPath);
+            downloadFiles(client, compilerUri, compilerPath);
+
+            unzipFiles(allegroPath, allegroUnzipPath);
+            unzipFiles(compilerPath, compilerUnzipPath);
+
+            createParentDirectory(allegroDestPath);
+            createParentDirectory(compilerDestPath);
+
+            moveDirectory(allegroSrcPath, allegroDestPath);
+            moveDirectory(compilerSrcPath, compilerDestPath);
+
+            copyCodeblocksConf();
+            patchCodeblocks();
+            copyBackCodeblocksConf();
+
+            Console.WriteLine("Finish. Press any key to close.");
+            Console.ReadLine();
+        }
+
+        private static string getCodeblocksDir() {
+            string appdata = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+            string codeblocksDir = Path.Combine(appdata, "CodeBlocks");
+            if (!Directory.Exists(codeblocksDir)) {
+                throw new DirectoryNotFoundException(codeblocksDir);
+            }
+            return codeblocksDir;
+        }
+
+
+        private static void copyCodeblocksConf() {
+            var codeblocksDir = getCodeblocksDir();
+            string configFile = Path.Combine(codeblocksDir, "default.conf");
+            Console.WriteLine("Copy {0} to {1}", configFile, @".\default.conf");
+            File.Copy(configFile, @".\default.conf", true);
+        }
+
+        private static void copyBackCodeblocksConf() {
+            var codeblocksDir = getCodeblocksDir();
+            string configFile = Path.Combine(codeblocksDir, "default.conf");
+            string configBak = Path.Combine(codeblocksDir, "default.conf.bak");
+            if (!File.Exists(configBak)) {
+                Console.WriteLine("Create backup of {0}", configFile);
+                File.Copy(configFile, configBak);
+            }
+            Console.WriteLine("Override {0}", configFile);
+            File.Copy(@".\default.conf.new", configFile, true);
+        }
+
+        private static void patchCodeblocks() {
+
+            XmlDocument doc = new XmlDocument();
+            doc.Load(@".\default.conf");
+
+            Console.WriteLine("Patch gcc settings.");
+            XmlDocument patchGccDoc = new XmlDocument();
+            patchGccDoc.LoadXml(PatchContext.gccPatch);
+
+            XmlNode node = doc.DocumentElement.SelectSingleNode("/CodeBlocksConfig/compiler/sets/gcc");
+            XmlNode parentNode = node.ParentNode;
+            parentNode.ReplaceChild(
+                parentNode.OwnerDocument.ImportNode(patchGccDoc.DocumentElement, true),
+                node
+                );
+
+            Console.WriteLine("Patch gdb settings.");
+            XmlDocument patchGdbDoc = new XmlDocument();
+            patchGdbDoc.LoadXml(PatchContext.gdbPatch);
+
+            node = doc.DocumentElement.SelectSingleNode("/CodeBlocksConfig/debugger_common/sets/gdb_debugger/conf1");
+            parentNode = node.ParentNode;
+            parentNode.ReplaceChild(
+                parentNode.OwnerDocument.ImportNode(patchGdbDoc.DocumentElement, true),
+                node
+                );
+
+            Console.WriteLine("Patch done.");
+
+            doc.Save(@".\default.conf.new");
+        }
+
+        private static void DirectoryCopy(string sourceDirName, string destDirName, bool copySubDirs) {
+            // Get the subdirectories for the specified directory.
+            DirectoryInfo dir = new DirectoryInfo(sourceDirName);
+
+            if (!dir.Exists) {
+                throw new DirectoryNotFoundException(
+                    "Source directory does not exist or could not be found: "
+                    + sourceDirName);
+            }
+
+            DirectoryInfo[] dirs = dir.GetDirectories();
+            // If the destination directory doesn't exist, create it.
+            if (!Directory.Exists(destDirName)) {
+                Directory.CreateDirectory(destDirName);
+            }
+
+            // Get the files in the directory and copy them to the new location.
+            FileInfo[] files = dir.GetFiles();
+            foreach (FileInfo file in files) {
+                string temppath = Path.Combine(destDirName, file.Name);
+                file.CopyTo(temppath, false);
+            }
+
+            // If copying subdirectories, copy them and their contents to new location.
+            if (copySubDirs) {
+                foreach (DirectoryInfo subdir in dirs) {
+                    string temppath = Path.Combine(destDirName, subdir.Name);
+                    DirectoryCopy(subdir.FullName, temppath, copySubDirs);
+                }
+            }
+        }
+
+        private static void moveDirectory(string src, string dest) {
+            string fullSrc = Path.GetFullPath(src);
+            string fullDest = Path.GetFullPath(dest);
+
+            if (Directory.Exists(fullDest)) {
+                Console.WriteLine("Delete old directory {0}", fullDest);
+                Directory.Delete(fullDest, true);
+            }
+            
+            if (Path.GetPathRoot(fullSrc) == Path.GetPathRoot(fullDest)) {
+                Console.WriteLine("Move {0} to {1}", fullSrc, fullDest);
+                Directory.Move(fullSrc, fullDest);
+                Console.WriteLine("Move done.");
+            } else {
+                Console.WriteLine("Copy {0} to {1}", fullSrc, fullDest);
+                DirectoryCopy(fullSrc, fullDest, true);
+                Console.WriteLine("Copy Done.");
+                Console.WriteLine("Delete {0}", fullSrc);
+                Directory.Delete(fullSrc, true);
+                Console.WriteLine("Delete done.");
+            }
+        }
+
+        private static void createParentDirectory(string dir) {
+            dir = Directory.GetParent(dir).FullName;
+            Console.WriteLine("Create path: {0}", dir);
+            Directory.CreateDirectory(dir);
+        }
+
+        private static void unzipFiles(string file, string path) {
+            string extractPath = Path.GetFullPath(path);
+
+            if (!extractPath.EndsWith(Path.DirectorySeparatorChar.ToString(), StringComparison.Ordinal))
+                extractPath += Path.DirectorySeparatorChar;
+
+            Console.WriteLine("Unzip {0} to {1}", file, extractPath);
+
+            ZipFile.ExtractToDirectory(file, extractPath);
+
+            Console.WriteLine("Unzip Done.");
+        }
+
+        private static void downloadFiles(WebClient client, string uri, string path) {
+            Console.WriteLine("Download {0} from {1}\n", path, uri);
+            if (!File.Exists(path + ".downloaded")) {
+                Task task = client.DownloadFileTaskAsync(uri, path);
+                currentFileName = path;
+                task.Wait();
+                Console.WriteLine();
+                File.Create(path + ".downloaded");
+            }
+            Console.WriteLine();
+            Console.WriteLine("Download complete.");
+        }
+
+        private static void Client_DownloadProgressChanged(object sender, DownloadProgressChangedEventArgs e) {
+            Console.Write("\rProgress: {0}/{1} ({2}%)", e.BytesReceived, e.TotalBytesToReceive, e.ProgressPercentage);
         }
     }
 }
